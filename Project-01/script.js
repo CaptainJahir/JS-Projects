b8ddb81485632c214c0d79706c4b93252c9f48b2
Kaiser <captainjahir007@gmail.com>
2024-09-16 14:14:00 +0530

    Project-01 =>  Added Functionality to Add Button and Enter Key in Inp Tag

--- Project-01/script.js
+++ Project-01/script.js
@@ -1,36 +1,54 @@
-let task = [
-    {
-        task : 'DO Dishes'
-    },
-    {
-        task : 'DO Coding'
-    },
-    {
-        task : 'Testing it'
-    },
-    {
-        task : 'Adding One More'
-    }
-];   // Here Goes The Task Array
+let task = [];   // Here Goes The Task Array
 
     /* Generating Html Through JS */
 
-
 let html = '';
 
-task.forEach((taskItem)=>{
-    html += `
-    
-    <div class="added-task">
-                <p class="task">${taskItem.task}</p>
-                <div class="task-action">
-                    <button class="done-task"><img src="Assests/correct.png" alt="task-done"></button>
-                    <button class="del-task"><img src="Assests/x-mark.png" alt="del-task"></button>
+function updateTask() {
+    task.forEach((taskItem)=>{
+        task = []; // Emptying The Array So that it won't print multiple times the previous task
+
+        html += `
+        
+        <div class="added-task">
+                    <p class="task">${taskItem.task}</p>
+                    <div class="task-action">
+                        <button class="done-task"><img src="Assests/correct.png" alt="task-done"></button>
+                        <button class="del-task"><img src="Assests/x-mark.png" alt="del-task"></button>
+                    </div>
                 </div>
-            </div>
-    
-    `;
-    
-    let taskDiv = document.body.querySelector('.all-tasks');
-    taskDiv.innerHTML = html;
-});
+        
+        `;
+        
+        let taskDiv = document.body.querySelector('.all-tasks');
+        taskDiv.innerHTML = html;
+    });
+
+    inpTag.value = '';
+}
+
+// Adding Add button Functionality
+
+let addBtn = document.body.querySelector('.add-task'); // Targeting Add Button
+let inpTag = document.body.querySelector('.inp-task'); // Targeting Input Tag
+
+// this function is for adding to multiple maddEventlistner 
+
+function EventListenerCode(){
+    /* Works When Input Tag Contains Value and Doesn't Work When Input tag Doesn't contain value */
+    if (inpTag.value) {
+        task.push({'task' : inpTag.value});
+        updateTask();
+    }
+    else{
+        alert('Please Enter The task');
+    }
+}
+
+addBtn.addEventListener('click' , () => {EventListenerCode()}); // This for click on add button
+
+inpTag.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        EventListenerCode();
+    }
+}); // This Will Trigger on Enter Button in Input Tag 
