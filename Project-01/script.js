4fb34fb6afea0e45db0ce7dbaeb79bb74455e719
Kaiser <captainjahir007@gmail.com>
2024-09-16 12:55:03 +0530

    Project-01 => Generating HTML Through Js

--- Project-01/script.js
+++ Project-01/script.js
@@ -1,17 +1,36 @@
-let addBtn = document.querySelector(".add-task"); // Add Button
-let inpTag = document.querySelector(".task"); // Input Tag
-
-/* Adding Add Button Function  */
-
-function addBtn(){
-    if (inpTag.value) {
-        inpTag.value
-    } else {
-        
+let task = [
+    {
+        task : 'DO Dishes'
+    },
+    {
+        task : 'DO Coding'
+    },
+    {
+        task : 'Testing it'
+    },
+    {
+        task : 'Adding One More'
     }
-}
+];   // Here Goes The Task Array
 
-addBtn.addEventListener("click" , (event) => {
-    alert('button clicked');
-    event.stopPropagation();
-})
+    /* Generating Html Through JS */
+
+
+let html = '';
+
+task.forEach((taskItem)=>{
+    html += `
+    
+    <div class="added-task">
+                <p class="task">${taskItem.task}</p>
+                <div class="task-action">
+                    <button class="done-task"><img src="Assests/correct.png" alt="task-done"></button>
+                    <button class="del-task"><img src="Assests/x-mark.png" alt="del-task"></button>
+                </div>
+            </div>
+    
+    `;
+    
+    let taskDiv = document.body.querySelector('.all-tasks');
+    taskDiv.innerHTML = html;
+});
