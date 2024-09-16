47ee1c2da612a8ef88fb46d45ec0196ae87aadc2
Kaiser <captainjahir007@gmail.com>
2024-09-16 18:05:27 +0530

    Project - 01  => Fixed Some Errors and Add Delete button Functionality

--- Project-01/script.js
+++ Project-01/script.js
@@ -1,12 +1,13 @@
-let task = [];   // Here Goes The Task Array
+let taskArr = [];   // Here Goes The Task Array
 
     /* Generating Html Through JS */
 
 let html = '';
 
 function updateTask() {
-    task.forEach((taskItem)=>{
-        task = []; // Emptying The Array So that it won't print multiple times the previous task
+    html =``; // This Removes the previous Tasks which already Exists (fixing duplicates)
+
+    taskArr.forEach((taskItem)=>{
 
         html += `
         
@@ -14,10 +15,9 @@ function updateTask() {
                     <p class="task">${taskItem.task}</p>
                     <div class="task-action">
                         <button class="done-task"><img src="Assests/correct.png" alt="task-done"></button>
-                        <button class="del-task"><img src="Assests/x-mark.png" alt="del-task"></button>
+                        <button class="del-task" onclick="delTask()"><img src="Assests/x-mark.png" alt="del-task"></button>
                     </div>
                 </div>
-        
         `;
         
         let taskDiv = document.body.querySelector('.all-tasks');
@@ -37,7 +37,7 @@ let inpTag = document.body.querySelector('.inp-task'); // Targeting Input Tag
 function EventListenerCode(){
     /* Works When Input Tag Contains Value and Doesn't Work When Input tag Doesn't contain value */
     if (inpTag.value) {
-        task.push({'task' : inpTag.value});
+        taskArr.push({'task' : inpTag.value});
         updateTask();
     }
     else{
@@ -52,3 +52,13 @@ inpTag.addEventListener('keydown', (event) => {
         EventListenerCode();
     }
 }); // This Will Trigger on Enter Button in Input Tag 
+
+function delTask() {
+    /* Removing From DOM */
+    let taskDiv = document.body.querySelector(".all-tasks");  // Targeting the task div container
+    let task = document.body.querySelector(".added-task");  // Targeting Actual Task
+    taskDiv.removeChild(task);
+
+    /* Removing From JS Array */
+    taskArr.pop(task);
+}
